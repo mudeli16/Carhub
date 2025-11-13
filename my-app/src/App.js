2700102ff63bd85dc869cf1b9408cac93d6932ae
mudeli16 <146018530+mudeli16@users.noreply.github.com>
2025-11-13 14:57:25 -0600

    Remove Home import from App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import AppRoutes from "./routes/AppRoutes";
-import Home from  "./pages/Home";
+
 
 
 
